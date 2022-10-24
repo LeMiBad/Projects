61943ad8b7391c2f845b992370a686991b20dac9
Leonid <79872121399.yandex@gmail.com>
2022-10-24 07:41:50 +0300

    anim card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react'
 import { Carousel } from 'react-responsive-carousel'
 import { Link } from 'react-router-dom'
 import { pickIndex } from '../../store/projectsModel'
@@ -8,9 +9,22 @@ const ProjectCard = (cardInfo: ProjectInfoProps) => {
     const {projectName, projectUrl, images, id} = cardInfo
 
 
+    const back = useRef() as React.MutableRefObject<HTMLDivElement>
+    const [animation, setAnimation] = useState(css.none)
+
+    useEffect(() => {
+        const scrollListener = (e: any) => {
+            if(back.current.offsetTop - window.innerHeight + 1 <= e.path[1].scrollY) {
+                setAnimation(css.enter)
+            }
+        }
+        document.addEventListener('scroll', scrollListener)
+        return () => document.removeEventListener('scroll', scrollListener)
+    }, [])
+
     return (
         <>
-            <div className={css.projectCard}>
+            <div ref={back} className={`${css.projectCard} ${animation}`}>
                 <div className={css.name}>{projectName}</div>
                 <Carousel infiniteLoop showStatus={false} showThumbs={false}>
                     {images.map((img, i) => <div key={i}><img alt='projectImg' src={img}></img></div>)}
